731bfff92adfd8a7925f854024097d7044effb3a
pedro49max <115159968+pedro49max@users.noreply.github.com>
2024-05-14 12:30:03 +0200

    Initial Code

---
+++ src/tp1/logic/AlienManager.java
@@ -0,0 +1,54 @@
+package tp1.logic;
+
+import tp1.logic.gameobjects.RegularAlien;
+
+public class AlienManager  {
+	
+	private Game game;
+	private int remainingAliens;
+	
+	//TODO fill with your code
+
+	public  GameObjectContainer initialize() {
+		this.remainingAliens = 0;
+		GameObjectContainer container = new GameObjectContainer();
+		
+		initializeOvni(container);
+		initializeRegularAliens(container);
+		initializeDestroyerAliens(container);
+		
+		//TODO fill with your code
+		
+		
+		return container;
+	}
+	
+	private void initializeOvni(GameObjectContainer container) {
+		//container.add(new Ufo(game));
+	}
+	
+	private void initializeRegularAliens (GameObjectContainer container) {
+
+		//TODO fill with your code
+		//		container.add(new RegularAlien(....));
+	}
+	
+	private void initializeDestroyerAliens(GameObjectContainer container) {
+		//TODO fill with your code
+	}
+
+	private void costumedInitialization(GameObjectContainer container, InitialConfiguration conf) {
+		for (String shipDescription : conf.getShipDescription()) {
+			String[] words = shipDescription.toLowerCase().trim().split("\\s+");
+			//AlienShip ship = ...
+			container.add(ship);
+			this.remainingAliens++;
+		}
+	}
+
+
+
+	//TODO fill with your code
+
+
+}
---
+++ src/tp1/logic/Level.java
@@ -0,0 +1,18 @@
+package tp1.logic;
+
+public enum Level {
+ 
+	;
+	
+	//TODO use your enum Level from p1
+	
+	public static String all(String separator) {
+		return null;
+	}
+
+	public static Level valueOfIgnoreCase(String string) {
+		return null;
+	}
+
+	
+}
